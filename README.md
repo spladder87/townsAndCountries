a9a0e85a37ee38043ec88890b3c083fd7a4cad5e
Emil Örberg <32409967+spladder87@users.noreply.github.com>
2019-10-01 00:27:54 +0200

    initial version with menu

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # townsAndCountries
 Städer och länder
+Inlämningen består i att göra en sida där dynamisk inhämtning och lagring av data används för att visualisera användarens besökta städer och länder.
+
+Den egna kodens funktion och eventuella förslag på förbättringar förklaras i en tillhörande resonerande textinlämning.
+
+land.json
+stad.json
+
+Kravspecifikation:
+
+De bifogade JSON-filerna ska användas.
+Sidan ska ha en meny som visar länder. Klickar besökaren på ett land, så ska detta landets städer presenteras i en egen vy eller undermeny.
+Klickar besökaren på en stad, så ska en informationsvy visas med fakta om staden och även stadens invånarantal.
+I informationsvyn över staden ska det finnas en knapp "besökt" som sparar stadens ID i en Array som även ska sparas till localStorage.
+I samma meny som visar länder ska det under länderna finnas en länk till en sida med titeln "Städer jag besökt".
+Sidan "Städer jag besökt" ska visa de städer besökaren markerat att den besökt och ska även räkna ut det totala antalet människor som besökaren kan ha råkat träffa (tips: Vet du summan av invånarantalet i de besökta städerna?).
+Besökaren ska även kunna rensa sin historik och tömma sin lista på besökta städer.
+ 
+
+När du är klar så skall du lämna in en textfil med länk till ditt repo samt en länk till en fungerande sida via github pages. Dokumentet skall även innehålla den reflekterande texten.
---
+++ script.js
@@ -0,0 +1,77 @@
+let navBar = document.getElementById('navbarCollapse');
+
+function readJson(url) {
+    // http://localhost:8080
+    fetch(url)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                    parseMenu(data);
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+
+
+function parseMenu(jsonObj) {
+    console.log(jsonObj);
+    for (let i = 0; i < jsonObj.length; i++) {
+        console.log(i);
+        navBar.innerHTML += navBar.innerHTML = `<ul class="nav navbar-nav">
+                  <li class="nav-item dropdown">
+                  <a href="#${jsonObj[i].id}" class="nav-link dropdown-toggle" data-toggle="dropdown">${jsonObj[i].countryname}</a>
+                  
+                  <div class="dropdown-menu"></div></li></ul>`;
+                  console.log(i);
+                  let stad = document.getElementByClassName("dropdown-menu");
+                  stad.innerHTML += stad.innerHTML = `<a href="#" class="dropdown-item">Inbox</a>`;
+                  console.log(i);
+                //   fetch("json/stad.json").then(function(response) {
+                //     return response.json();
+                //   }).then(function(json) {
+                //     // do a bunch of stuff
+                //   }
+    }
+}
+
+//parseMenu();
+
+readJson("json/land.json");
+
+
+
+
+
+
+
+
+{/* <ul class="nav navbar-nav">
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Messages</a>
+    <div class="dropdown-menu">
+        <a href="#" class="dropdown-item">Inbox</a>
+        <a href="#" class="dropdown-item">Drafts</a>
+        <a href="#" class="dropdown-item">Sent Items</a>
+        <div class="dropdown-divider"></div>
+        <a href="#"class="dropdown-item">Trash</a>
+    </div>
+</li>
+</ul>
+
+<ul class="nav navbar-nav ml-auto">
+<li class="nav-item">
+    <a href="#" class="nav-link">Visited places</a>
+</li>
+</ul>
+ */}
