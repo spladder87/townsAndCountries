2eaa12fce708c6e5bf8d985f41772de503b3a7c0
e <e>
2019-10-02 15:31:39 +0200

    This version is fully working according to spec
    Todo: refactoring and clean up log messages

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let navBar = document.getElementById('navbarCollapse');
 
+var visitedTowns = [];
+
 function readJson(url) {
-    // http://localhost:8080
     fetch(url)
         .then(
             function (response) {
@@ -13,7 +14,21 @@ function readJson(url) {
                 // Examine the text in the response
                 response.json().then(function (data) {
                     console.log(data);
-                    parseMenu(data);
+                    for (let i = 0; i < data.length; i++) {
+                        console.log(i);
+                        navBar.innerHTML += navBar.innerHTML = `<ul class="nav navbar-nav">
+                                                                <li class="nav-item dropdown">
+                                                                <a id="${data[i].id}" onClick="getTowns(this.id)" class="nav-link dropdown-toggle" data-toggle="dropdown">${data[i].countryname}</a>
+                                                                <div id="div${data[i].id}" div class="dropdown-menu"></div>
+                                                                </li>
+                                                                </ul>`;
+                    }
+                    let navUl = document.getElementById('visitedNavlink');
+                    navUl.innerHTML += `<ul class="nav navbar-nav">
+                                        <li class="nav-item">
+                                        <a id="visitedPlaces" onClick="visitedTownView()" class="nav-link">Besökta städer</a>
+                                        </li>
+                                        </ul>`;
                 });
             }
         )
@@ -23,62 +38,83 @@ function readJson(url) {
 }
 
 
+function getTowns(id) {
+    let towns = document.getElementById("div" + id);
 
-function parseMenu(jsonObj) {
-    console.log(jsonObj);
-    for (let i = 0; i < jsonObj.length; i++) {
-        console.log(i);
-        navBar.innerHTML += navBar.innerHTML = `<ul class="nav navbar-nav">
-                  <li class="nav-item dropdown">
-                  <a href="#${jsonObj[i].countryname}" id="${jsonObj[i].id}" onClick="getTowns(this.id)" class="nav-link dropdown-toggle" data-toggle="dropdown">${jsonObj[i].countryname}</a><div id="div${jsonObj[i].id}" div class="dropdown-menu" onClick="alert(this.id)"></div></li></ul>`;
-                //   console.log(i);
-                //   let stad = document.getElementByClassName("dropdown-menu");
-                //   stad.innerHTML += stad.innerHTML = `<a href="#" class="dropdown-item">Inbox</a>`;
-                //   console.log(i);
-                //   fetch("json/stad.json").then(function(response) {
-                //     return response.json();
-                //   }).then(function(json) {
-                //     // do a bunch of stuff
-                //   }
-    }
+    fetch("json/stad.json").then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        // do a bunch of stuff
+        console.log(json);
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].countryid == id) {
+                towns.innerHTML += towns.innerHTML = `<a id="${json[i].id}" onClick="setView(this.id)" class="dropdown-item">${json[i].stadname}</a>`;
+            }
+        }
+    })
+
 }
 
-//parseMenu();
-
-function getTowns (id){
-    console.log(id);
-    let towns = document.getElementById("div"+id);
-    console.log(towns);
-    towns.innerHTML = "hej";
-    //get
-    //id.append = '<div class="dropdown-menu">Hej</div>';
-//<div class="dropdown-menu"></div>
+function setView(viewID) {
+    let townView = document.getElementById("townView");
+    console.log(viewID);
+    fetch("json/stad.json").then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        // do a bunch of stuff
+        townView.innerHTML = "";
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].id == viewID) {
+                townView.innerHTML += townView.innerHTML = `<button type="button" class="btn btn-success" onClick="setVisitedTown(${json[i].id})">Besökt</button>
+                                                            <h2>${json[i].stadname}</h2>
+                                                            <p>Population = ${json[i].population}</p>`;
+            }
+        }
+    })
 }
+
+function setVisitedTown(townID) {
+
+    visitedTowns.push(JSON.stringify(townID));
+    console.log(visitedTowns);
+    localStorage.setItem('townVisited', JSON.stringify(visitedTowns));
+    console.log(localStorage);
+
+    console.log(visitedTowns);
+    console.log(typeof (visitedTowns));
+}
+
+function visitedTownView() {
+    var townView = document.getElementById('townView');
+    var peopleTotal = 0;
+    var storedString = localStorage.getItem('townVisited');
+    visitedTowns = JSON.parse(storedString);
+    visitedTowns = [...new Set(visitedTowns)];
+    console.log(visitedTowns);
+
+    fetch("json/stad.json").then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        // do a bunch of stuff
+        townView.innerHTML = `<h2>Besökta Städer</h2>
+                              <p id="peopleTotal"></p>
+                              <ul id="visitedTownUl"></ul>
+                              <button type="button" class="btn btn-success" onClick="localStorage.clear(),visitedTownView()">Rensa localstorage</button>`;
+        var visitedLi = document.getElementById('visitedTownUl');
+        console.log(json);
+        for (let i = 0; i < json.length; i++) {
+            for (let x = 0; x < visitedTowns.length; x++) {
+                if (json[i].id == visitedTowns[x]) {
+                    console.log(json[i].population);
+                    peopleTotal += json[i].population;
+                    visitedLi.innerHTML += visitedLi.innerHTML = `<li>${json[i].stadname}</li>`;
+                }
+            }
+
+        }
+        document.getElementById('peopleTotal').innerHTML = `Antal människor du träffat = ${peopleTotal}`;
+    })
+}
+
 readJson("json/land.json");
 
-
-
-
-
-
-
-
-{/* <ul class="nav navbar-nav">
-<li class="nav-item dropdown">
-    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Messages</a>
-    <div class="dropdown-menu">
-        <a href="#" class="dropdown-item">Inbox</a>
-        <a href="#" class="dropdown-item">Drafts</a>
-        <a href="#" class="dropdown-item">Sent Items</a>
-        <div class="dropdown-divider"></div>
-        <a href="#"class="dropdown-item">Trash</a>
-    </div>
-</li>
-</ul>
-
-<ul class="nav navbar-nav ml-auto">
-<li class="nav-item">
-    <a href="#" class="nav-link">Visited places</a>
-</li>
-</ul>
- */}
